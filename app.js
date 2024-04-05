4e179c64e6273c4f87be11f1da90fa388f6c1ccc
Humza920 <humza.rehman.779@gmail.com>
2024-04-05 13:58:56 -0700

    HALF

--- app.js
+++ app.js
@@ -3,3 +3,13 @@
 // Question 1
 // var age = 17
 // alert("My age is " + age)
+
+// Question 2
+// BAAD MAI DAIKHAI GI
+
+// Question 3
+// var birthYear = 2006
+// var type = "Data type of my birth year is " + typeof(birthYear)
+// document.write("My birth year is " + birthYear + "</br>" + type)
+
+// Question 4
